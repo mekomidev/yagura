781f69b52fbefb98e4f0c7bb31c356ff8b75958e
James Kerber <kerber.jg@gmail.com>
2019-01-21 01:20:23 +0100

    Defined Yagura and Overlay

---
+++ src/overlay.ts
@@ -0,0 +1,38 @@
+import { deepFreeze } from './utils/objectUtils';
+import { YaguraEvent } from './event';
+
+export interface OverlayConfig {
+    name: string;
+    version: string;
+    vendor: string;
+    yaguraVersion?: string;
+
+    strategy: any;
+    overlay: any;
+}
+
+export abstract class Overlay {
+    public readonly config: OverlayConfig;
+
+    /**
+     * Initializes the Overlay
+     *
+     * @param {OverlayConfig} config Overlay configuration object. Should be a plain JSON object, any functions will be excluded
+     */
+    constructor(config: OverlayConfig) {
+        // Deep copy config (this excludes functions!) and deep freeze it
+        this.config = deepFreeze(JSON.parse(JSON.stringify(config)));
+    }
+
+    /** Handles incoming events */
+    public abstract async handleEvent(e: YaguraEvent): Promise<void>;
+
+    /**
+     * Called whenever an unhandled error is thrown in the app
+     *
+     * @param err
+     */
+    public async handleError(err: Error) {
+        // overriding is optional
+    }
+}
---
+++ src/yagura.ts
@@ -0,0 +1,137 @@
+import { Overlay, OverlayConfig } from './overlay';
+import { Module } from './module';
+import { YaguraError, StubError } from './utils/errors';
+import { Logger, DefaultLogger } from './modules/logger.module';
+
+import _colors = require('colors');
+import { YaguraEvent } from './event';
+
+export class Yagura {
+    private static _overlay: Overlay;
+    private static logger: Logger;
+
+    public static async start(overlay: Overlay) {
+        if (Yagura._overlay) {
+            throw new YaguraError(`Yagura has already been initialized, you cannot call start() more than once`);
+        }
+
+        // Mount handlers
+        process.on('beforeExit', async () => {
+            await this._handleShutdown();
+        });
+
+        process.on('unhandledRejection', async (err) => {
+            await this.handleError(err);
+        });
+
+        process.on('uncaughtException', async (err) => {
+            await this.handleError(err);
+        });
+
+        // Initialize dependencies
+        Yagura.logger = new DefaultLogger();
+
+        // Initialize Overlay
+        Yagura._overlay = overlay;
+
+        // Connect to DB ?
+        // await this._connectDatabase();
+
+        // Start overlay
+    }
+
+    /*
+     *  Event subsystem
+     */
+    public static handleEvent(event: YaguraEvent) {
+        // TODO: prevent events from being handled more than once
+        // TODO: add analytics
+        // TODO: apply filters
+        this._overlay.handleEvent(event);
+    }
+
+    /*
+     *  Modules subsystem
+     */
+    private _modules: { [name: string]: ModuleHolder<any> } = {};
+    // {
+    //     "name": {
+    //         active: Module,
+    //         vendors: {
+    //             "default": Module,
+    //             "vendor1": Module
+    //         }
+    //     }
+    // }
+
+    public getModule<M extends Module>(name: string, vendor?: string): M {
+        const m: ModuleHolder<M> = this._modules[name];
+
+        if (!m) {
+            return null;
+        } else {
+            if (vendor) {
+                if (!m.vendors[vendor]) {
+                    return m.vendors[vendor];
+                } else {
+                    return null;
+                }
+            } else {
+                return m.active;
+            }
+        }
+    }
+
+    /**
+     * Returns a Module proxy object, which will always store the reference to the active instance of the requested Module
+     *
+     * @param name name of the Module to be adapted
+     * @returns {Module} a Module proxy for the requested Module
+     */
+    public getModuleProxy<M extends Module>(name: string): M {
+        throw new StubError();
+        return null;
+    }
+
+    public registerModule<M extends Module>(mod: M): void {
+        let m: ModuleHolder<M> = this._modules[mod.name];
+
+        if (!m) {
+            m = {
+                active: mod,
+                vendors: {
+                    [mod.vendor]: mod
+                }
+            };
+
+            m.vendors[mod.vendor] = mod;
+
+            this._modules[mod.name] = m;
+        } else {
+            if (m.vendors[mod.vendor]) {
+                throw new YaguraError(`Module '${mod.name}' has already been registered for vendor '${mod.vendor}'`);
+            } else {
+                m.active = mod;
+                m.vendors[mod.vendor] = mod;
+            }
+        }
+    }
+
+    public static async handleError(err: Error) {
+        // TODO: prevent errors from being handled more than once
+        this.logger.error(err.toString());
+
+        if (!!this._overlay) {
+            await this._overlay.handleError(err);
+        }
+    }
+
+    private static async _handleShutdown() {
+        this.logger.info('Shutting down...');
+    }
+}
+
+interface ModuleHolder<M extends Module> {
+    active: M;
+    vendors: { [name: string]: M };
+}
