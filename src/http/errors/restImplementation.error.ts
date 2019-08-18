38c214aafd5afad4689b0eaec7cb0140a4fadf98
James Kerber <kerber.jg@gmail.com>
2019-08-18 02:31:36 +0200

    Added RestImplementationError
    If you wanna be really pedantic...

---
+++ src/http/errors/restImplementation.error.ts
@@ -0,0 +1,15 @@
+import { HttpRequest } from "../httpServer.overlay";
+
+export class RestImplementationError extends Error {
+    public readonly method: string;
+    public readonly path: string;
+
+    constructor(event: HttpRequest, comment?: string) {
+        super();
+
+        this.method = event.req.method;
+        this.path = event.req.originalUrl;
+
+        this.message = `The implementation of ${this.method} ${this.path} does not meet REST standards; ` + comment || '';
+    }
+}
