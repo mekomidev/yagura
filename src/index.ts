4269d4651e55776c4c018107f209ae7e54ca5a0d
James Kerber <kerber.jg@gmail.com>
2019-09-11 17:57:57 +0200

    Fixed missing framework exports

--- src/index.ts
+++ src/index.ts
@@ -2,18 +2,24 @@
 import { Yagura } from './framework/yagura';
 import { Overlay } from './framework/overlay';
 import { Service } from './framework/service';
+import { YaguraEvent, eventFilter } from './framework/event';
 
 import { YaguraError, VersionMismatchError } from './utils/errors';
 import { TimeoutError, promiseTimeout } from './utils/promise.utils';
 import { HandleGuard } from './utils/handleGuard';
+
+import { Logger, DefaultLogger } from './services/logger.service';
 
 export {
     // Framework
     Yagura,
     Overlay,
     Service,
+    YaguraEvent,
     // Errors
     YaguraError, VersionMismatchError, TimeoutError,
+    // Services
+    Logger, DefaultLogger,
     // Utils
-    promiseTimeout, HandleGuard
+    eventFilter, promiseTimeout, HandleGuard
 };
