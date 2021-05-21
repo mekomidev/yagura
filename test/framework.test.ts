b37e991e705d6c25ba7845b111fac40614e096a4
James Kerber <kerber.jg@gmail.com>
2021-05-21 12:19:57 +0200

    Implemented all tests

--- test/framework.test.ts
+++ test/framework.test.ts
@@ -1,6 +1,6 @@
 /* eslint-disable @typescript-eslint/no-unused-vars */
 /* eslint-disable @typescript-eslint/require-await */
-import { Yagura, Layer, Service, YaguraEvent, YaguraError, eventFilter } from '../src';
+import { Yagura, Layer, Service, YaguraEvent, YaguraError, ErrorHandler, eventFilter } from '../src';
 import { ServerEvent, ServerEventType } from '../src/framework/server.event';
 
 import 'mocha';
@@ -13,18 +13,18 @@ describe('Framework', () => {
         expect(app).to.be.instanceOf(Yagura);
     });
 
-    describe('Service handling', () => {
-        class DummyService extends Service {
-            constructor(vendor: string = 'test') {
-                super('Dummy', vendor);
-            }
-
-            public text: string = 'world';
-            public hello(): string {
-                return this.text;
-            }
+    class DummyService extends Service {
+        constructor(vendor: string = 'test') {
+            super('Dummy', vendor);
         }
 
+        public text: string = 'world';
+        public hello(): string {
+            return this.text;
+        }
+    }
+
+    describe('Service handling', () => {
         class BetterDummyService extends DummyService {
             constructor() {
                 super('cool');
@@ -33,19 +33,18 @@ describe('Framework', () => {
             public text: string = "cool world";
         }
 
-        let app: Yagura;
+        // let app: Yagura;
 
         it('should register, mount and initialize a service', async () => {
-            app = await Yagura.start([]);
+            const app = await Yagura.start([]);
             const service: Service = new DummyService();
 
             // check if initialized
-            const fake = sinon.fake.resolves(null);
-            sinon.replace(service, 'initialize', fake);
+            const spy = sinon.spy(service, 'initialize');
 
             await app.registerService(service);
 
-            expect(fake.called, "Service not initialized").to.be.eq(true);
+            expect(spy.called, "Service not initialized").to.be.eq(true);
 
             // check if mounted
             expect((service as any).yagura, "Service not mounted").to.be.eq(app);
@@ -54,36 +53,52 @@ describe('Framework', () => {
             expect(app.getService('Dummy'), "Service not registered").to.be.eq(service);
         });
 
-        it('should fail to mount a Service twice', () => {
-            const service: Service = app.getService('Dummy');
-
-            try {
-                service.mount(app);
-                expect.fail();
-            } catch (e) {
-                expect(e).to.be.instanceOf(YaguraError);
-            }
-        })
-
-        it('should retrieve a registered service', () => {
+        it('should retrieve a registered service', async () => {
+            const app = await Yagura.start([], [new DummyService()]);
             const service: Service = app.getService('Dummy');
 
             expect(service).to.be.instanceOf(DummyService);
         });
 
-        it('should return null when asking for a non-existent Service');
+        it('should fail to mount a Service twice', async () => {
+            const app = await Yagura.start([], [new DummyService()]);
+            const service: Service = app.getService('Dummy');
 
-        it('should return null when asking for a non-existent Service\'s proxy');
+            try {
+                await service.initialize(app);
+                expect.fail();
+            } catch (e) {
+                expect(e).to.be.instanceOf(YaguraError);
+            }
+        });
 
-        it('should retrieve a registered service by vendor', () => {
+        it('should return null when asking for a non-existent Service', async () => {
+            const app = await Yagura.start([]);
+
+            const service = app.getService('Dummy');
+            expect(service).to.be.eq(null);
+        });
+
+        it('should return null when asking for a non-existent Service\'s proxy', async () => {
+            const app = await Yagura.start([]);
+
+            const proxy = app.getServiceProxy('Dummy');
+            expect(proxy).to.be.eq(null);
+        });
+
+        it('should retrieve a registered service by vendor', async () => {
+            const app = await Yagura.start([], [new DummyService()]);
             const service: Service = app.getService('Dummy', 'test');
 
             expect(service).to.be.instanceOf(DummyService);
         });
 
         it('should update the active service when registering a new vendor', async () => {
-            const service: Service = new BetterDummyService();
-            await app.registerService(service);
+            const app = await Yagura.start([]);
+            await app.registerService(new DummyService());
+            await app.registerService(new BetterDummyService());
+
+            const service: Service = app.getService('Dummy');
 
             expect(app.getService('Dummy'), "New service not active").to.be.eq(service);
             expect(app.getService('Dummy', 'cool'), "New service not available by vendor").to.be.eq(service);
@@ -92,9 +107,7 @@ describe('Framework', () => {
 
         describe('Service proxy', () => {
             it('should return a service proxy', async () => {
-                app = await Yagura.start([]);
-
-                await app.registerService(new DummyService());
+                const app = await Yagura.start([], [new DummyService()]);
                 const proxy: DummyService = app.getServiceProxy('Dummy');
 
                 // check basic access
@@ -112,19 +125,63 @@ describe('Framework', () => {
                 expect(proxy.text, "Doesn't allow read access to its property").to.be.eq('cool world');
             });
 
-            it('should return existing properties');
+            it('should return existing properties', async () => {
+                const app = await Yagura.start([], [new DummyService()]);
+                const proxy: DummyService = app.getServiceProxy('Dummy');
 
-            it('should not return non-existing properties');
+                expect(proxy.text).to.not.be.eq(undefined);
+            });
 
-            it('should set existing properties');
+            it('should not return non-existing properties', async () => {
+                const app = await Yagura.start([], [new DummyService()]);
+                const proxy: DummyService = app.getServiceProxy('Dummy');
 
-            it('should not set non-existing properties');
+                expect((proxy as any).text2).to.be.eq(undefined);
+            });
 
-            it('should return prototype');
+            it('should set existing properties', async () => {
+                const app = await Yagura.start([], [new DummyService()]);
+                const proxy: DummyService = app.getServiceProxy('Dummy');
 
-            it('should return whether is extensible');
+                proxy.text = 'hello2';
 
-            it('should be able to prevent extensions');
+                expect(proxy.text).to.be.eq('hello2');
+            });
+
+            it('should not set non-existing properties', async () => {
+                const app = await Yagura.start([], [new DummyService()]);
+                const proxy: DummyService = app.getServiceProxy('Dummy');
+
+                try { (proxy as any).text2 = 'hello2'; }
+                catch(e) { expect(e).to.be.instanceOf(TypeError); }
+
+                expect((proxy as any).text2).to.not.be.eq('hello2');
+            });
+
+            it('should return prototype', async () => {
+                const app = await Yagura.start([], [new DummyService()]);
+                const proxy: DummyService = app.getServiceProxy('Dummy');
+
+                expect(proxy.constructor.prototype).to.be.eq(DummyService.prototype);
+            });
+
+            it('should return whether is extensible', async () => {
+                const service = new DummyService();
+                const app = await Yagura.start([], [service]);
+                const proxy: DummyService = app.getServiceProxy('Dummy');
+
+                expect(Object.isExtensible(proxy)).to.be.eq(Object.isExtensible(service));
+            });
+
+            it('should be able to prevent extensions', async () => {
+                const service = new DummyService();
+                const app = await Yagura.start([], [service]);
+                const proxy: DummyService = app.getServiceProxy('Dummy');
+
+                Object.preventExtensions(service);
+
+                expect(Object.isExtensible(proxy)).to.be.eq(Object.isExtensible(service));
+            });
         });
     });
 
@@ -225,24 +282,24 @@ describe('Framework', () => {
 
     describe('Initialization', () => {
         it('should initialize services at start', async () => {
-            class DummyService extends Service {
-                constructor() {
-                    super('Dummy', 'test');
-                }
-
-                public async initialize() { /* */ }
-            }
-
             const service = new DummyService();
-            const fake = sinon.fake.resolves(null);
-            sinon.replace(service, 'initialize', fake);
+            const spy = sinon.spy(service, 'initialize');
 
             await Yagura.start([], [service]);
 
-            expect(fake.called, "Service not initialized").to.be.eq(true);
+            expect(spy.called, "Service not initialized").to.be.eq(true);
         });
-        it('should initialize layers at start');
-        it('should dispatch a ServerEvent of type `start`');
+
+        it('should dispatch a ServerEvent of type `start`', async () => {
+            const spy = sinon.spy(Yagura.prototype, 'dispatch');
+            await Yagura.start([]);
+
+            spy.restore();
+
+            expect(spy.called, "Dispatch not called").to.be.eq(true);
+            expect(spy.args[0][0], "Not a ServerEvent").to.be.instanceOf(ServerEvent);
+            expect((spy.args[0][0] as ServerEvent).data, "Not value 'start'").to.be.eq(ServerEventType.start);
+        });
     });
 
     describe('Event handling', () => {
@@ -350,18 +407,116 @@ describe('Framework', () => {
 
             process.env.NODE_ENV = 'test';
         });
-        it('should consume event if none of the layers don\'t');
+
+        it('should consume event if none of the layers don\'t', async () => {
+            const app = await Yagura.start([]);
+            const event = new DummyEvent({ hello: 'world' });
+
+            // fake consume
+            const fake = sinon.fake.resolves(null);
+            sinon.replace(event, 'consume', fake);
+
+            await app.dispatch(event);
+
+            expect(fake.called).to.be.eq(true);
+        });
     });
 
     describe('Error handling', () => {
-        it('should catch errors at stack initialization');
-        it('should catch errors at Services\' initialization');
-        it('should catch errors when event handling by Layer fails');
-        it('should catch errors when error handling by Layer fails');
-        it('should catch errors when forced consumption of Event fails');
-        it('should throw error when dispatching event before initialization');
-        it('should handle given error');
-        it('should not throw any other errors itself');
-        it('should not handle the same error more than once');
+        class ErrorInitLayer extends Layer {
+            async onInitialize() {
+                console.log('hello');
+            }
+
+            public handleEvent(e: YaguraEvent): Promise<YaguraEvent> {
+                console.log('hello');
+                return null;
+            }
+
+            public async handleError(err: Error) {
+                console.error('error handled: ' + err.name);
+            }
+        }
+
+        it('should handle given error', async () => {
+            const app = await Yagura.start([]);
+
+            const errorHandler = app.getService<ErrorHandler>('ErrorHandler');
+            const fake = sinon.fake.resolves(null);
+            sinon.replace(errorHandler, 'handle', fake);
+
+            await app.handleError(new Error('hello'));
+        });
+
+        it('should re-throw errors at stack initialization', async () => {
+            const layer = new ErrorInitLayer();
+            const fake = sinon.fake.throws(new Error('error thrown'));
+            sinon.replace(layer, 'onInitialize', fake);
+
+            const spy = sinon.spy(Yagura.prototype, 'handleError');
+
+            try { await Yagura.start([layer]); }
+            catch (e) { expect(e).to.be.instanceOf(Error) }
+
+            spy.restore();
+            expect(spy.called).to.be.eq(true);
+        });
+
+        it('should catch errors at Services\' initialization', async () => {
+            const service = new DummyService();
+            const fake = sinon.fake.throws(new Error('service init fail'));
+            sinon.replace(service, 'initialize', fake);
+
+
+            const spy = sinon.spy(Yagura.prototype, 'handleError');
+            try { await Yagura.start([], [service]); }
+            catch (e) { expect(e).to.be.instanceOf(Error) }
+
+            sinon.restore();
+            expect(spy.called).to.be.eq(true);
+        });
+
+        it('should redirect error handling to Layer when event consumption fails', async () => {
+            const layer = new ErrorInitLayer();
+            const fake = sinon.fake.throws(new Error('error thrown'));
+            sinon.replace(layer, 'handleEvent', fake);
+
+            const spy = sinon.spy(layer, 'handleError');
+
+            await Yagura.start([layer]);
+
+            spy.restore();
+            expect(spy.called).to.be.eq(true);
+        });
+
+        it('should catch errors when error handling by Layer fails', async () => {
+            const layer = new ErrorInitLayer();
+            const fake = sinon.fake.throws(new Error('error thrown'));
+            sinon.replace(layer, 'handleEvent', fake);
+            sinon.replace(layer, 'handleError', fake);
+
+            const spy = sinon.spy(Yagura.prototype, 'handleError');
+
+            await Yagura.start([layer]);
+
+            spy.restore();
+            expect(spy.called).to.be.eq(true);
+        });
+
+        it('should catch errors when forced consumption of Event fails', async () => {
+            const fake = sinon.fake.throws(new Error('error thrown'));
+            sinon.replace(ServerEvent.prototype, 'consume', fake);
+
+            const spy = sinon.spy(Yagura.prototype, 'handleError');
+
+            await Yagura.start([]);
+
+            sinon.restore();
+            expect(spy.called).to.be.eq(true);
+        });
+
+        afterEach(() => {
+            sinon.restore();
+        })
     });
 });
