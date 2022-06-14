7bef35b4079607c79ab0339f7035d7ce8af6e457
James Kerber <kerber.jg@gmail.com>
2022-06-14 12:52:58 +0200

    Fixed promiseTimeout blocking process

--- src/utils/promise.utils.ts
+++ src/utils/promise.utils.ts
@@ -1,17 +1,27 @@
 class PromiseTimeoutError extends Error {}
 
 export const promiseTimeout = async function(ms: number, promise: Promise<any>, throwError: boolean = true) {
-    const timeout = new Promise((resolve) => {
-        const id = setTimeout(() => {
-            clearTimeout(id);
-            resolve(new PromiseTimeoutError(`Promise timed out in ${ms}ms`));
+    let timeoutId: NodeJS.Timeout;
+    const timeout = new Promise((resolve, reject) => {
+        timeoutId = setTimeout(() => {
+            reject(new PromiseTimeoutError(`Promise timed out in ${ms}ms`));
         }, ms);
     });
 
-    const result = await Promise.race([promise, timeout]);
-    if (result instanceof PromiseTimeoutError) {
-        if (throwError) {
-            throw result;
+    let result: any;
+    let error: Error;
+    try {
+        result = await Promise.race([promise, timeout]);
+    } catch (e) {
+        error = e;
+    }
+
+    clearTimeout(timeoutId);    // always clear timeout
+    timeoutId.unref();
+
+    if(!!error) {
+        if(!(error instanceof PromiseTimeoutError) || throwError) {
+            throw error;
         } else {
             return null;
         }
